bb24d0f40f869dce830b61b9fad6117bdb81c584
Alexander Henka <Alexander.Henka@googlemail.com>
2017-05-24 14:26:28 +0200

    Initial Commit

---
+++ src/core.js
@@ -0,0 +1,176 @@
+/**
+ * Copyright (c) 2017 Alexander Henka. All rights reserved.
+ * Use of this source code is governed by a Apache 2.0 license that can be
+ * found in the LICENSE file.
+ */
+
+
+/**
+ * Get the current URL.
+ *
+ * @param {function(string)} callback - called when the URL of the current tab
+ *   is found.
+ */
+function getCurrentTabUrl(callback) {
+  // Query filter to be passed to chrome.tabs.query - see
+  // https://developer.chrome.com/extensions/tabs#method-query
+  var queryInfo = {
+    active: true,
+    currentWindow: true
+  };
+
+  chrome.tabs.query(queryInfo, function(tabs) {
+    // chrome.tabs.query invokes the callback with a list of tabs that match the
+    // query. When the popup is opened, there is certainly a window and at least
+    // one tab, so we can safely assume that |tabs| is a non-empty array.
+    // A window can only have one active tab at a time, so the array consists of
+    // exactly one tab.
+    var tab = tabs[0];
+
+    // A tab is a plain object that provides information about the tab.
+    // See https://developer.chrome.com/extensions/tabs#type-Tab
+    var url = tab.url;
+
+    // tab.url is only available if the "activeTab" permission is declared.
+    // If you want to see the URL of other tabs (e.g. after removing active:true
+    // from |queryInfo|), then the "tabs" permission is required to see their
+    // "url" properties.
+    console.assert(typeof url === 'string', 'tab.url should be a string');
+
+    callback(url);
+  });
+
+  // Most methods of the Chrome extension APIs are asynchronous. This means that
+  // you CANNOT do something like this:
+  //
+  // var url;
+  // chrome.tabs.query(queryInfo, function(tabs) {
+  //   url = tabs[0].url;
+  // });
+  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+}
+
+function allowDrop(ev) {
+    console.log('allowDrop = ' +ev);
+
+    ev.preventDefault();
+}
+
+function drag(ev) {
+
+    console.log('drag = ' +ev);
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+    console.log('ev.dataTransfer = ' +ev.dataTransfer.getData("text"));
+}
+
+function drop(ev) {
+
+    /*
+    console.dir('ev = '+ ev.dataTransfer);
+    console.dir(ev.dataTransfer);
+
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+
+    ev.target.appendChild(document.getElementById(data));
+*/  ev.preventDefault();
+
+    console.log("Drop");
+
+    // Get the data, which is the id of the drop target
+    var data = ev.dataTransfer.getData("text");
+    console.log('data = ' + data);
+
+    ev.target.appendChild(document.getElementById(data));
+    // Clear the drag data cache (for all formats/types)
+    ev.dataTransfer.clearData();
+}
+
+function t(){
+    console.log('HAHAHA');
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+""
+    /*
+    document.querySelector('#yes-drop').addEventListener('drag', drag);
+    document.querySelector('#outer-dropzone').addEventListener('dragover', allowDrop);
+    document.querySelector('#outer-dropzone').addEventListener('drop', drop);
+    document.querySelector('#outer-dropzone').addEventListener("dragenter", function( event ) {
+        // highlight potential drop target when the draggable element enters it
+        if ( event.target.className == "dropzone" ) {
+            event.target.style.background = "purple";
+        }
+
+    }, false); */
+
+    var dragged;
+    /* events fired on the draggable target */
+    document.querySelector('#yes-drop').addEventListener("drag", function( event ) {
+
+
+
+    }, false);
+
+    document.querySelector('#yes-drop').addEventListener("dragstart", function( event ) {
+        // store a ref. on the dragged elem
+        dragged = event.target;
+        event.dataTransfer.setData("text/plain", event.target.id);
+        console.log('ev.dataTransfer = ' +event.dataTransfer.getData("text/plain"));
+        // make it half transparent
+        event.target.style.opacity = .5;
+    }, false);
+
+    document.querySelector('#yes-drop').addEventListener("dragend", function( event ) {
+        // reset the transparency
+        event.target.style.opacity = "";
+    }, false);
+
+    /* events fired on the drop targets */
+    document.querySelector('#outer-dropzone').addEventListener("dragover", function( event ) {
+        // prevent default to allow drop
+        event.preventDefault();
+    }, false);
+
+    document.querySelector('#outer-dropzone').addEventListener("dragenter", function( event ) {
+        // highlight potential drop target when the draggable element enters it
+        if ( event.target.className == "dropzone" ) {
+            event.target.style.background = "purple";
+        }
+
+    }, false);
+    document.querySelector('#outer-dropzone').addEventListener("dragleave", function( event ) {
+        // reset background of potential drop target when the draggable element leaves it
+        if ( event.target.className == "dropzone" ) {
+            event.target.style.background = "";
+        }
+
+    }, false);
+
+    document.querySelector('#outer-dropzone').addEventListener("drop", function( event ) {
+        // prevent default action (open as link for some elements)
+        event.preventDefault();
+        // move dragged elem to the selected drop target
+        if ( event.target.className == "dropzone" ) {
+            event.target.style.background = "";
+            dragged.parentNode.removeChild( dragged );
+            event.target.appendChild( dragged );
+
+            window.close();
+        }
+
+    }, false);
+
+
+    getCurrentTabUrl(function(url) {
+            console.log('Tab URL = '+url);
+        chrome.tabs.executeScript(
+            {
+                file: 'js/injector.js'
+            });
+
+
+        });
+
+});
---
+++ src/injector.js
@@ -0,0 +1,178 @@
+/**
+ * Created by alex on 12.05.17.
+ */
+
+function messUpwords() {
+
+    console.log('window.jQuery = ' +window.jQuery);
+
+    if(!window.jQuery) {
+
+        var v='3.1.1';
+
+        var done=false;
+
+        console.log('window.jQuery = ' +window.jQuery);
+
+        var script=document.createElement('script');
+
+        script.onload = script.onreadystatechange = function(){
+            if(!done&&(!script.readyState||script.readyState==='loaded'||script.readyState==='complete')){
+                console.log('readyState = ' +script.readyState);
+                done=true;
+                dyslexia();
+            }
+        };
+
+        script.src='//code.jquery.com/jquery-'+v+'.min.js';
+        document.getElementsByTagName('head')[0].appendChild(script)
+
+    }else{
+       // alert("jqzery is da");
+        dyslexia();
+    }
+
+    /*
+    var v='3.1.1';
+    if(window.jQuery===undefined||window.jQuery.fn.jquery<v) {
+
+    }else{
+        dyslexia();
+    }
+
+*/
+    function dyslexia() {
+
+      //  alert("Execute dsylexisa");
+       // alert(window.jQuery);
+    var getTextNodesIn = function(el) {
+
+        return $(el).find(":not(iframe,script)").addBack().contents().filter(function() {
+            return this.nodeType === 3;
+        });
+    };
+
+   // var textNodes = document.querySelectorAll("p, h1, h2, h3");
+    var textNodes = getTextNodesIn($("*"));
+
+    function isLetter(char) {
+        return /^[\d]$/.test(char);
+    }
+
+    var wordsInTextNodes = [];
+    for (var i = 0; i < textNodes.length; i++) {
+        var node = textNodes[i];
+
+        var words = []
+
+        var re = /\w+/g;
+        var match;
+        while ((match = re.exec(node.nodeValue)) != null) {
+
+            var word = match[0];
+            var position = match.index;
+
+            words.push({
+                length: word.length,
+                position: position
+            });
+        }
+
+        wordsInTextNodes[i] = words;
+    }
+
+
+    function messUpWords () {
+        for (var i = 0; i < textNodes.length; i++) {
+
+            var node = textNodes[i];
+
+            for (var j = 0; j < wordsInTextNodes[i].length; j++) {
+
+                // Only change a tenth of the words each round.
+                if (Math.random() > 1/10) {
+
+                    continue;
+                }
+
+                var wordMeta = wordsInTextNodes[i][j];
+
+                var word = node.nodeValue.slice(wordMeta.position, wordMeta.position + wordMeta.length);
+                var before = node.nodeValue.slice(0, wordMeta.position);
+                var after  = node.nodeValue.slice(wordMeta.position + wordMeta.length);
+
+                node.nodeValue = before + messUpWord(word) + after;
+            }
+        }
+    }
+
+    function messUpWord (word) {
+        if (word.length < 3) {
+            return word;
+        }
+        return word[0] + messUpMessyPart(word.slice(1, -1)) + word[word.length - 1];
+    }
+
+    function messUpMessyPart (messyPart) {
+        if (messyPart.length < 2) {
+            return messyPart;
+        }
+
+        var a, b;
+        while (!(a < b)) {
+            a = getRandomInt(0, messyPart.length - 1);
+            b = getRandomInt(0, messyPart.length - 1);
+        }
+
+        return messyPart.slice(0, a) + messyPart[b] + messyPart.slice(a+1, b) + messyPart[a] + messyPart.slice(b+1);
+    }
+
+    // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    setInterval(messUpWords, 50);
+}
+
+}
+
+/* events fired on the drop targets */
+document.querySelector('html').addEventListener("dragover", function( event ) {
+    // prevent default to allow drop
+    event.preventDefault();
+}, false);
+
+document.querySelector('html').addEventListener("dragenter", function( event ) {
+        // highlight potential drop target when the draggable element enters it
+        // if ( event.target.className == "dropzone" ) {
+        //   event.target.style.border = "6px dashed red";
+        // event.target.style.opacity = "1.0";
+        //}
+
+}, false);
+
+document.querySelector('html').addEventListener("dragleave", function( event ) {
+    // highlight potential drop target when the draggable element enters it
+    // if ( event.target.className == "dropzone" ) {
+   // event.target.style.background = "green";
+    //event.target.style.opacity = "0.5";
+    //}
+
+}, false);
+
+
+
+document.querySelector('html').addEventListener("drop", function( event ) {
+   // alert(window.jQuery);
+    // prevent default action (open as link for some elements)
+    event.preventDefault();
+    console.dir('event ='+event);
+    console.dir(event);
+
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+
+  //  console.log('data = '+data);
+    messUpwords();
+
+}, false);
